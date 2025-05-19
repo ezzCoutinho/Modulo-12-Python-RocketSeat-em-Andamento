b3d7ed979008735824f205eb0674e2f212b00778
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-19 20:23:24 -0300

    Adiciona a classe RabbitMQConsumer para consumir mensagens do RabbitMQ, incluindo configuração de conexão e método de callback para processamento de mensagens recebidas.

---
+++ src/main/rabbitmq_configs/consumer.py
@@ -0,0 +1,47 @@
+import pika
+from src.configs.credentials import get_credentials
+
+credentials = get_credentials()
+
+
+class RabbitMQConsumer:
+    def __init__(self) -> None:
+        self.__host = credentials["host"]
+        self.__port = credentials["port"]
+        self.__username = credentials["username"]
+        self.__password = credentials["password"]
+        self.__queue = credentials["queue"]
+        self.__channel = self.create_channel()
+
+    def create_channel(self):
+        connection_parameters = pika.ConnectionParameters(
+            host=self.__host,
+            port=self.__port,
+            credentials=pika.PlainCredentials(
+                username=self.__username,
+                password=self.__password,
+            ),
+        )
+
+        channel = pika.BlockingConnection(connection_parameters).channel()
+        channel.queue_declare(
+            queue=self.__queue,
+            durable=True,
+        )
+        channel.basic_consume(
+            queue=self.__queue,
+            auto_ack=True,
+            on_message_callback=self.rabbitmq_callback,
+        )
+        return channel
+
+    def start(self):
+        print("Sistema conectado ao RabbitMQ")
+        self.__channel.start_consuming()
+
+    def rabbitmq_callback(self, ch, method, properties, body):
+        print(f"Mensagem recebida: {body}")
+
+
+rabbitmq_consumer = RabbitMQConsumer()
+rabbitmq_consumer.start()
